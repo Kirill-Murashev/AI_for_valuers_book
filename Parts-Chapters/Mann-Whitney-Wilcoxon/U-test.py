3f2aba07c724648a456d125c4bfe361e1ace220c
Kirill Murashev <kirill.murashev@gmail.com>
2022-05-28 21:29:14 +0300

    Python subsection in progress

--- Parts-Chapters/Mann-Whitney-Wilcoxon/U-test.py
+++ Parts-Chapters/Mann-Whitney-Wilcoxon/U-test.py
@@ -10,19 +10,19 @@ from scipy.stats import norm
 import scipy.stats as stats
 
 # import dataset
-df = pd.read_csv  ("spba-flats-210928.csv")
-print (df)
+df = pd.read_csv("spba-flats-210928.csv")
+print(df)
 type(df["price_m"])
 
-# get only prices and counties
+# get only prices and counties, release RAM
 df1 = df[["price_m", "county"]]
+del [[df]]
 
 # calculate the number of observations on data frame
-spbaLen = len(df1.index)
-spbaLenR = round(math.sqrt(spbaLen))
+spbaLenR = round(math.sqrt(len(df1.index)))
 
 # fit a normal distribution to the data: mean and standard deviation
-mu, std = norm.fit(df1["price_m"]) 
+mu, std = norm.fit(df1["price_m"])
 
 # plot the histogram
 plt.hist(df1["price_m"], bins=spbaLenR, density=True)
@@ -36,67 +36,67 @@ plt.plot(x, p, 'k', linewidth=2)
 title = "Fit Values: {:.2f} and {:.2f}".format(mu, std)
 plt.title(title)
 
+plt.savefig('spba-price-histogram-py.pdf')
+
 # create separate data frames for city and suburbs
-dfs = df1[df1["county"].str.startswith('s')]
-dfl = df1[df1["county"].str.startswith('l')]
+#dfs = df1[df1["county"].str.startswith('s')]
+#dfl = df1[df1["county"].str.startswith('l')]
 
 # Saint-Petersburg
 
 # calculate the number of observations on data frame
-spbLen = len(dfs.index)
-spbLenR = round(math.sqrt(spbLen))
+#spbLen = len(dfs.index)
+#spbLenR = round(math.sqrt(spbLen))
 
 # fit a normal distribution to the data: mean and standard deviation
-muS, stdS = norm.fit(dfs["price_m"]) 
+#muS, stdS = norm.fit(dfs["price_m"])
 
 # plot the histogram
-plt.hist(dfs["price_m"], bins=spbLenR, density=True)
+#plt.hist(dfs["price_m"], bins=spbLenR, density=True)
 
 # plot the PDF
-xmin, xmax = plt.xlim()
-x = np.linspace(xmin, xmax, 100)
-ps = norm.pdf(x, muS, stdS)
+#xmin, xmax = plt.xlim()
+#x = np.linspace(xmin, xmax, 100)
+#ps = norm.pdf(x, muS, stdS)
 
-plt.plot(x, ps, 'k', linewidth=2)
-title = "Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
-plt.title(title)
+#plt.plot(x, ps, 'k', linewidth=2)
+#title = "Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
+#plt.title(title)
 
 # LO
 # calculate the number of observations on data frame
-loLen = len(dfl.index)
-loLenR = round(math.sqrt(loLen))
+#loLen = len(dfl.index)
+#loLenR = round(math.sqrt(loLen))
 
 # fit a normal distribution to the data: mean and standard deviation
-muL, stdL = norm.fit(dfl["price_m"]) 
+#muL, stdL = norm.fit(dfl["price_m"])
 
 # plot the histogram
-plt.hist(dfl["price_m"], bins=loLenR, density=True)
+#plt.hist(dfl["price_m"], bins=loLenR, density=True)
 
 # plot the PDF
-xmin, xmax = plt.xlim()
-x = np.linspace(xmin, xmax, 100)
-pl = norm.pdf(x, muL, stdL)
+#xmin, xmax = plt.xlim()
+#x = np.linspace(xmin, xmax, 100)
+#pl = norm.pdf(x, muL, stdL)
 
-plt.plot(x, ps, 'k', linewidth=2)
-title = "Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
-plt.title(title)
+#plt.plot(x, ps, 'k', linewidth=2)
+#title = "Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
+#plt.title(title)
 
 # add labels to data
-dfs["region"] = "SPb"
-dfl["region"] = "LO"
+#dfs["region"] = "SPb"
+#dfl["region"] = "LO"
 
 # plot boxplot
-prices = [dfs, dfl]
-allPrices = pd.concat(prices)
-plt.figure()
-allPrices.boxplot(by="region")
+#prices = [dfs, dfl]
+#allPrices = pd.concat(prices)
+#plt.figure()
+#allPrices.boxplot(by="region")
 
 # normality tests
-shapiroWSpb, shapiroPvalueSpb = stats.shapiro(dfs['price_m'])
-shapiroWLO, shapiroPvalueLO = stats.shapiro(dfl['price_m'])
+#shapiroWSpb, shapiroPvalueSpb = stats.shapiro(dfs['price_m'])
+#shapiroWLO, shapiroPvalueLO = stats.shapiro(dfl['price_m'])
 
 # Mann-Whitney test
-U = stats.mannwhitneyu(x=dfs['price_m'], y=dfl['price_m'],
-                       alternative = 'two-sided')
-
-
+#U = stats.mannwhitneyu(x=dfs['price_m'], y=dfl['price_m'],
+#                       alternative='two-sided')
