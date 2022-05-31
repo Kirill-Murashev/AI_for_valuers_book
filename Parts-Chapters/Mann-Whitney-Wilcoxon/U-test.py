612e917c0e59e00db7695c36ac5e636e7e1f811b
Kirill Murashev <kirill.murashev@gmail.com>
2022-05-31 15:43:31 +0300

    add almaty dataset

--- Parts-Chapters/Mann-Whitney-Wilcoxon/U-test.py
+++ Parts-Chapters/Mann-Whitney-Wilcoxon/U-test.py
@@ -8,6 +8,13 @@ import math
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import scipy.stats as stats
+from scipy.stats import normaltest
+from scipy.stats import shapiro
+from scipy.stats import anderson
+from scipy.stats import mannwhitneyu
+
+# set significance level
+alpha = 0.05
 
 # import dataset
 df = pd.read_csv("spba-flats-210928.csv")
@@ -36,67 +43,148 @@ plt.plot(x, p, 'k', linewidth=2)
 title = "Fit Values: {:.2f} and {:.2f}".format(mu, std)
 plt.title(title)
 
+# save to .pdf
 plt.savefig('spba-price-histogram-py.pdf')
 
 # create separate data frames for city and suburbs
-#dfs = df1[df1["county"].str.startswith('s')]
-#dfl = df1[df1["county"].str.startswith('l')]
+dfs = df1[df1["county"].str.startswith('s')]
+dfl = df1[df1["county"].str.startswith('l')]
 
 # Saint-Petersburg
 
 # calculate the number of observations on data frame
-#spbLen = len(dfs.index)
-#spbLenR = round(math.sqrt(spbLen))
+spbLenR = round(math.sqrt(len(dfs.index)))
 
 # fit a normal distribution to the data: mean and standard deviation
-#muS, stdS = norm.fit(dfs["price_m"])
+muS, stdS = norm.fit(dfs["price_m"])
 
 # plot the histogram
-#plt.hist(dfs["price_m"], bins=spbLenR, density=True)
+plt.hist(dfs["price_m"], bins=spbLenR, density=True)
 
 # plot the PDF
-#xmin, xmax = plt.xlim()
-#x = np.linspace(xmin, xmax, 100)
-#ps = norm.pdf(x, muS, stdS)
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+ps = norm.pdf(x, muS, stdS)
 
-#plt.plot(x, ps, 'k', linewidth=2)
-#title = "Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
-#plt.title(title)
+plt.plot(x, ps, 'k', linewidth=2)
+title = "S-Pb. Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
+plt.title(title)
+
+# save to .pdf
+plt.savefig('spb-price-histogram-py.pdf')
 
 # LO
+
 # calculate the number of observations on data frame
-#loLen = len(dfl.index)
-#loLenR = round(math.sqrt(loLen))
+loLenR = round(math.sqrt(len(dfl.index)))
 
 # fit a normal distribution to the data: mean and standard deviation
-#muL, stdL = norm.fit(dfl["price_m"])
+muL, stdL = norm.fit(dfl["price_m"])
 
 # plot the histogram
-#plt.hist(dfl["price_m"], bins=loLenR, density=True)
+plt.hist(dfl["price_m"], bins=loLenR, density=True)
 
 # plot the PDF
-#xmin, xmax = plt.xlim()
-#x = np.linspace(xmin, xmax, 100)
-#pl = norm.pdf(x, muL, stdL)
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+pl = norm.pdf(x, muL, stdL)
 
-#plt.plot(x, ps, 'k', linewidth=2)
-#title = "Fit Values: {:.2f} and {:.2f}".format(muS, stdS)
-#plt.title(title)
+plt.plot(x, pl, 'k', linewidth=2)
+title = "LO. Fit Values: {:.2f} and {:.2f}".format(muL, stdL)
+plt.title(title)
+
+# save to .pdf
+plt.savefig('lo-price-histogram-py.pdf')
 
 # add labels to data
-#dfs["region"] = "SPb"
-#dfl["region"] = "LO"
+dfs["region"] = "SPb"
+dfl["region"] = "LO"
 
 # plot boxplot
-#prices = [dfs, dfl]
-#allPrices = pd.concat(prices)
-#plt.figure()
-#allPrices.boxplot(by="region")
+prices = [dfs, dfl]
+allPrices = pd.concat(prices)
+plt.figure()
+allPrices.boxplot(by="region")
+
+# save to .pdf
+plt.savefig('spb-lo-boxplot-py.pdf')
 
 # normality tests
-#shapiroWSpb, shapiroPvalueSpb = stats.shapiro(dfs['price_m'])
-#shapiroWLO, shapiroPvalueLO = stats.shapiro(dfl['price_m'])
+
+# Shapiro-Wilk
+
+# SPB
+stat, p = shapiro(dfs['price_m'])
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+if p < alpha:
+    print('Sample does not look Gaussian (reject H0)')
+else:
+    print('Sample looks Gaussian (fail to reject H0)')
+
+# LO
+stat, p = shapiro(dfl['price_m'])
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+if p < alpha:
+    print('Sample does not look Gaussian (reject H0)')
+else:
+    print('Sample looks Gaussian (fail to reject H0)')
+
+# D'Agostino K^2
+
+# SPB
+stat, p = normaltest(dfs['price_m'])
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+if p < alpha:
+    print('Sample does not look Gaussian (reject H0)')
+else:
+    print('Sample looks Gaussian (fail to reject H0)')
+
+# LO
+stat, p = normaltest(dfl['price_m'])
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+if p < alpha:
+    print('Sample does not look Gaussian (reject H0)')
+else:
+    print('Sample looks Gaussian (fail to reject H0)')
+
+# Anderson-Darling
+
+# SPB
+result = anderson(dfs['price_m'])
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+    sl, cv = result.significance_level[i], result.critical_values[i]
+    if result.statistic < result.critical_values[i]:
+        print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+    else:
+        print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+
+# LO
+result = anderson(dfl['price_m'])
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+    sl, cv = result.significance_level[i], result.critical_values[i]
+    if result.statistic < result.critical_values[i]:
+        print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+    else:
+        print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+
+# shapiroWSpb, shapiroPvalueSpb = stats.shapiro(dfs['price_m'])
+# shapiroWLO, shapiroPvalueLO = stats.shapiro(dfl['price_m'])
 
 # Mann-Whitney test
-#U = stats.mannwhitneyu(x=dfs['price_m'], y=dfl['price_m'],
+stat, p = mannwhitneyu(dfs['price_m'], dfl['price_m'])
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p < 0.05:
+    print('Probably different distributions')
+else:
+    print('Probably the same distribution')
+
+# U = stats.mannwhitneyu(x=dfs['price_m'], y=dfl['price_m'],
 #                       alternative='two-sided')
---
+++ Parts-Chapters/Mann-Whitney-Wilcoxon/U-test.r
@@ -0,0 +1,3 @@
+# Mann-Whitney-U-test, also known as Mann-Whitney-Wilcoxon test
+# Not to be confused with Wilcoxon signed-rank test for dependent samples
+
