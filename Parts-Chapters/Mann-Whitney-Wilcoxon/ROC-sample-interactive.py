f9a39693e90be81e71ada71789b893acf21cfdd7
Kirill Murashev <kirill.murashev@gmail.com>
2022-06-10 16:32:52 +0300

    ROC-AUC in progress

--- Parts-Chapters/Mann-Whitney-Wilcoxon/ROC-sample-interactive.py
+++ Parts-Chapters/Mann-Whitney-Wilcoxon/ROC-sample-interactive.py
@@ -5,13 +5,17 @@ Created on Thu Jun  9 20:30:30 2022
 
 @author: kaarlahti
 """
+
+# Import Libraries
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
+# Plot
 f0 = stats.norm(0, 1)
 f1 = stats.norm(2, 1)
 fig, ax = plt.subplots()
-xi = plt.linspace(-2, 5, 100)
+xi = np.linspace(-2, 5, 100)
 ax.plot(xi, f0.pdf(xi), label=r'$f(x|C_0)$')
 ax.plot(xi, f1.pdf(xi), label=r'$f(x|C_1)$')
 ax.legend(fontsize=16, loc=(1, 0))
@@ -19,3 +23,27 @@ ax.set_xlabel(r'$x$', fontsize=18)
 ax.vlines(0, 0, ax.axis()[-1] * 1.1, linestyles='--', lw=3.)
 ax.fill_between(xi, f1.pdf(xi), where=xi > 0, alpha=.3, color='g')
 ax.fill_between(xi, f0.pdf(xi), where=xi > 0, alpha=.3, color='b')
+
+# save to .pdf
+plt.savefig('Plot-ROC-step-1.pdf', bbox_inches='tight')
+
+from IPython.html.widgets import interact
+
+def plot_roc_interact(c=0):
+    xi = np.linspace(-3,5,100)
+    fig,axs = plt.subplots(1,2)
+    fig.set_size_inches((10,3))
+    ax = axs[0]
+    ax.plot(xi,f0.pdf(xi),label=r'$f(x|C_0)$')
+    ax.plot(xi,f1.pdf(xi),label=r'$f(x|C_1)$')
+    ax.set_xlabel(r'$x$',fontsize=18)
+    ax.vlines(c,0,ax.axis()[-1]*1.1,linestyles='--',lw=3.)
+    ax.fill_between(xi,f1.pdf(xi),where=xi>c,alpha=.3,color='g')
+    ax.fill_between(xi,f0.pdf(xi),where=xi>c,alpha=.3,color='b')
+    ax.axis(xmin=-3,xmax=5)
+    crange = np.linspace(-3,5,50)
+    ax=axs[1]
+    ax.plot(1-f0.cdf(crange),1-f1.cdf(crange))
+    ax.plot(1-f0.cdf(c),1-f1.cdf(c),'o',ms=15.)
+    ax.set_xlabel('False-alarm probability')
+    ax.set_ylabel('Detection probability')
